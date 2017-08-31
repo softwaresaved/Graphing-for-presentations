8066b1ab4a6ca714579c18136880b42d152b79f4
SimonHettrick <s.hettrick@software.ac.uk>
2017-08-31 20:32:04 +0100

    Starting to add line chartså

--- graphing.py
+++ graphing.py
@@ -39,9 +39,7 @@ def import_csv_to_df(filename):
 
 def plot_bar_matplot(df, current_chart):
     """
-    Create a basic plot for each question. Plots of more specific interest will
-    be created in a separate function, because it's impossible to automate it.
-    Uses Seaborn to try and make things prettier
+    Create a basic plot for each question.
     :params: a dict of dataframe, the imported plot details
     :return: A list of saved charts
     """
@@ -49,19 +47,19 @@ def plot_bar_matplot(df, current_chart):
     percent_symbol = '%'
 
     # Get the chart params from the lookup table
-    filename = plot_details[current_chart][0]
-    y_axis_name = plot_details[current_chart][1]
-    x_axis_label = plot_details[current_chart][2]
-    x_axis_label_rotation = plot_details[current_chart][3]
-    x_axis_label_cutoff = plot_details[current_chart][4]
-    y_axis_label = plot_details[current_chart][5]
-    chart_title = plot_details[current_chart][6]
-    show_values = plot_details[current_chart][7]
-    skip_labels = plot_details[current_chart][8]
-    bottom_size = plot_details[current_chart][9]
-    title_size = plot_details[current_chart][10]
-    body_size = plot_details[current_chart][11]
-    label_size = plot_details[current_chart][12]
+    filename = plot_details[current_chart][1]
+    y_axis_name = plot_details[current_chart][2]
+    x_axis_label = plot_details[current_chart][3]
+    x_axis_label_rotation = plot_details[current_chart][4]
+    x_axis_label_cutoff = plot_details[current_chart][5]
+    y_axis_label = plot_details[current_chart][6]
+    chart_title = plot_details[current_chart][7]
+    show_values = plot_details[current_chart][8]
+    skip_labels = plot_details[current_chart][9]
+    bottom_size = plot_details[current_chart][10]
+    title_size = plot_details[current_chart][11]
+    body_size = plot_details[current_chart][12]
+    label_size = plot_details[current_chart][13]
 
     # Set the labels
     labels = df.index
@@ -97,16 +95,14 @@ def plot_bar_matplot(df, current_chart):
              fontsize=label_size)           # Set font size
 
     if chart_title != False:
-        plt.title(chart_title, fontname=global_specs['font_name'], fontsize=title_size, y=1.08)  # y increases the spacing between the title and
-                                                                                                 #plot content
+        plt.title(chart_title, fontname=global_specs['font_name'], fontsize=title_size, y=1.08)  # y increases the spacing between the title
+                                                                                                 # and plot content
 
     # Make plot scale to fit plot area
     plt.tight_layout()
 
     # Use the bespoke labels, and rotate them if necessary
-    
     fig.set_xticklabels(labels, rotation=x_axis_label_rotation, fontname=global_specs['font_name'], fontsize=body_size)
-
 
     # Turn off the spines
     fig.spines['left'].set_visible(False)
@@ -145,7 +141,100 @@ def plot_bar_matplot(df, current_chart):
     plt.clf()
 
     return
+
+
+def plot_line_matplot(df, current_chart):
+    """
+    Create a basic plot for each question. Plots of more specific interest will
+    be created in a separate function, because it's impossible to automate it.
+    Uses Seaborn to try and make things prettier
+    :params: a dict of dataframe, the imported plot details
+    :return: A list of saved charts
+    """
     
+    filename = plot_details[current_chart][1]
+    y_axis_name = plot_details[current_chart][2]
+    x_axis_label = plot_details[current_chart][3]
+    x_axis_label_rotation = plot_details[current_chart][4]
+    x_axis_label_cutoff = plot_details[current_chart][5]
+    y_axis_label = plot_details[current_chart][6]
+    chart_title = plot_details[current_chart][7]
+    show_values = plot_details[current_chart][8]
+    skip_labels = plot_details[current_chart][9]
+    bottom_size = plot_details[current_chart][10]
+    title_size = plot_details[current_chart][11]
+    body_size = plot_details[current_chart][12]
+    label_size = plot_details[current_chart][13]
+
+
+    # Set the labels
+    labels = df.index
+
+    # If labels are long, wrap 'em
+    labels = [ '\n'.join(wrap(l, x_axis_label_cutoff)) for l in labels ] # Change the number to change the max number of characters per line
+
+    if skip_labels != False:
+        count = 0
+        for x in range(0,len(labels)):
+            if count%(skip_labels+1) != 0:
+                labels[count]=''
+            count+=1
+    
+    fig = df[y_axis_name].plot(kind='line',                      # Plot a bar chart
+                legend=False,                                   # Turn the Legend off
+                xticks = range(0,len(df)),
+                figsize=(global_specs['plot_width'],global_specs['plot_height']))
+
+    if chart_title != False:
+        plt.title(chart_title, fontname=global_specs['font_name'], fontsize=title_size, y=1.08)  # y increases the spacing between the title
+                                                                                                 # and plot content
+
+    # Make plot scale to fit plot area
+    plt.tight_layout()
+
+
+
+
+    # Use the bespoke labels, and rotate them if necessary
+    fig.set_xticklabels(labels, rotation=x_axis_label_rotation, fontname=global_specs['font_name'], fontsize=body_size)
+
+    # Turn off the spines
+    fig.spines['right'].set_visible(False)
+    fig.spines['top'].set_visible(False)
+
+    # Read in the axis classes that may be used in the following
+    # if statements to set axis-related stuff
+    x_axe_class = fig.axes.get_xaxis()
+    y_axe_class = fig.axes.get_yaxis()
+
+    # X axis title
+    if x_axis_label == False:
+        x_axe_class.label.set_visible(False)    #Turn off x axis title
+    else:
+        fig.set_xlabel(x_axis_label)
+
+    # Y axis title
+    if y_axis_label == False:
+        y_axe_class.label.set_visible(False)    #Turn off y axis title
+    else:
+        fig.set_ylabel(y_axis_label)
+
+    # Remove the y-axis stuff
+    y_axe_class.set_visible(True)  
+
+    # Make gap at bottom bigger for labels
+    plt.subplots_adjust(bottom=bottom_size)
+
+
+
+
+    # Show the figure
+    plt.show()
+    # Clear figure so that parameters can be set clean by next figure
+    plt.clf()
+
+    return
+
     
 def main():
     """
@@ -155,13 +244,18 @@ def main():
     # Go through all charts in the lookup table
     for current_chart in plot_details:
         # Get the current chart name
-        filename = plot_details[current_chart][0]
+        filename = plot_details[current_chart][1]
         # Read survey data from csv
         df = import_csv_to_df(DATASTORE + filename)
         # Set the first column as the index
         df.set_index('answers', inplace=True)
         # Plot
-        plot_bar_matplot(df, current_chart)
+        if plot_details[current_chart][0] == 'line':
+            plot_line_matplot(df, current_chart)
+        else:
+            plot_bar_matplot(df, current_chart)
+
+
 
 if __name__ == '__main__':
     main()
